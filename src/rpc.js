5445062396507caf73849f9dcf05163dbb964e5f
Vijay Suthar <suthar67@gmail.com>
2024-11-04 17:16:31 +0530

    main: Move code to git repo

---
+++ src/rpc.js
@@ -0,0 +1,96 @@
+const amqplib = require("amqplib");
+const { v4: uuid4 } = require("uuid");
+
+let amqplibConnection = null;
+
+const getChannel = async () => {
+    if (amqplibConnection === null) {
+        amqplibConnection = await amqplib.connect("amqp://localhost");
+    }
+    return await amqplibConnection.createChannel();
+};
+
+const expensiveDBOperation = (payload, fakeResponse) => {
+    console.log(payload);
+    console.log(fakeResponse);
+
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res(fakeResponse);
+        }, 3000);
+    });
+};
+
+const RPCObserver = async (RPC_QUEUE_NAME, fakeResponse) => {
+    const channel = await getChannel();
+    await channel.assertQueue(RPC_QUEUE_NAME, {
+        durable: false,
+    });
+    channel.prefetch(1);
+    channel.consume(
+        RPC_QUEUE_NAME,
+        async (msg) => {
+            if (msg.content) {
+                // DB Operation
+                const payload = JSON.parse(msg.content.toString());
+                const response = await expensiveDBOperation(payload, fakeResponse); // call fake DB operation
+
+                channel.sendToQueue(
+                    msg.properties.replyTo,
+                    Buffer.from(JSON.stringify(response)),
+                    {
+                        correlationId: msg.properties.correlationId,
+                    }
+                );
+                channel.ack(msg);
+            }
+        },
+        {
+            noAck: false,
+        }
+    );
+};
+
+const requestData = async (RPC_QUEUE_NAME, payload, uuid) => {
+    const channel = await getChannel();
+
+    const q = await channel.assertQueue("", { exclusive: true });
+
+    channel.sendToQueue(RPC_QUEUE_NAME, Buffer.from(JSON.stringify(payload)), {
+        replyTo: q.queue,
+        correlationId: uuid
+    });
+
+    return new Promise((resolve, reject) => {
+        // timeout n
+        const timeout = setTimeout(() => {
+            channel.close();
+            resolve("API could not fullfil the request!");
+        }, 8000);
+        channel.consume(
+            q.queue,
+            (msg) => {
+                if (msg.properties.correlationId === uuid) {
+                    resolve(JSON.parse(msg.content.toString()));
+                    clearTimeout(timeout);
+                } else {
+                    reject("Data Not found!")
+                }
+            },
+            {
+                noAck: true,
+            }
+        );
+    });
+};
+
+const RPCRequest = async (RPC_QUEUE_NAME, requestPayload) => {
+    const uuid = uuid4(); //correlationId
+    return await requestData(RPC_QUEUE_NAME, requestPayload, uuid);
+};
+
+module.exports = {
+    getChannel,
+    RPCObserver,
+    RPCRequest
+}
